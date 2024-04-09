a6b521d24c8a51af9bf19185def5ae01f5fca58f
one1erp <avigaile@one1.co.il>
2024-04-09 09:06:34 +0300

    Initial commit with additional files

---
+++ One1.Controls/CustomMessageBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace One1.Controls
+{
+    public static class CustomMessageBox
+    {
+      public static void  Show(string text, MessageBoxButtons messageBoxButtons=MessageBoxButtons.OK, MessageBoxIcon messageBoxIcon=MessageBoxIcon.None)
+      {
+          MessageBox.Show(text, "Nautilus", messageBoxButtons,messageBoxIcon, MessageBoxDefaultButton.Button1, MessageBoxOptions.RtlReading);
+      }
+    }
+}
---
+++ One1.Controls/RedBorderGroupBox.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls.UI;
+
+namespace One1.Controls
+{
+    public class RedBorderGroupBox : RadGroupBox
+    {
+        private bool redBorder;
+
+        public bool RedBorder
+        {
+            get { return redBorder; }
+
+
+        }
+        public void RemoveRedBorder()
+        {
+            redBorder = false;
+            Invalidate();
+        }
+        public void SetRedBorder()
+        {
+            redBorder = true;
+            Invalidate();
+        }
+
+  
+
+         protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            if (redBorder)
+            {
+                ButtonBorderStyle b = ButtonBorderStyle.Dotted;
+                base.OnPaint(e);
+                ControlPaint.DrawBorder(e.Graphics, ClientRectangle,
+                                        Color.Red, 2, b,
+                                        Color.Red, 2, b,
+                                        Color.Red, 2, b,
+                                        Color.Red, 2, b);
+
+
+            }
+        }
+    }
+}
